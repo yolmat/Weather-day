80a1c0bccb3a38540ca7fe4596d293a3f52410ad
Mateus <mateus.saraiva09@gmail.com>
2023-03-17 00:30:14 -0300

    API Consumo

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 const apiKey = "0aa04cb08f8c1f6a8c7b36e01c080cef"
 
-let countryAcronyms
-const apiCountryURL = `https://flagsapi.com/${countryAcronyms}/flat/64.png`
-
 const cityInput = document.querySelector("#cityInput")
 const searchBtn = document.querySelector("#search")
 
@@ -11,7 +8,7 @@ const tempElement = document.querySelector("#temperature span")
 const descElement = document.querySelector("#description")
 const weatherIconElement = document.querySelector("#weatherIcon")
 const countryElement = document.querySelector("#country")
-const umidityElement = document.querySelector("#umidity span")
+const humidityElement = document.querySelector("#humidity span")
 const windElement = document.querySelector("#wind span")
 
 // Functions
@@ -22,12 +19,21 @@ const getWeatherData = async (city) => {
     const res = await fetch(apiWeatherURL)
     const data = await res.json()
 
-    console.log(data)
+    return data
 }
 
-const showWeatherData = city => {
-    getWeatherData(city)
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city)
+
+    cityElement.innerText = data.name
+    tempElement.innerText = parseInt(data.main.temp)
+    descElement.innerText = data.weather[0].description
+    weatherIconElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`)
+    humidityElement.innerText = `${data.main.humidity}%`
+    windElement.innerText = `${data.wind.speed}km/h`
 }
+
 // Events
 
 searchBtn.addEventListener("click", (e) => {
